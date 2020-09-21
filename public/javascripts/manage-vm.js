0cdcaef93afeaea46cc81dcaa4e79f50b947fde3
Bruno Costa <brunovasquescosta@gmail.com>
2020-09-21 13:52:20 +0100

    Add services to all external call to server

--- public/javascripts/manage-vm.js
+++ public/javascripts/manage-vm.js
@@ -1,16 +1,16 @@
 $('document').ready(function(){
   $('button.startVM').click(function(){
     $.ajax({
-      url:"/vm/start",
+      url:"/services/vm/start",
       success:function(data,textStatus,jqXHR){
         $('p.statusVM').text(data)
       }
-
+z
     })
   })
   $('button.statusVM').click(function(){
     $.ajax({
-      url:"/vm/status",
+      url:"/services/vm/status",
       success:function(data,textStatus,jqXHR){
         $('p.statusVM span.status').text(data)
       }
